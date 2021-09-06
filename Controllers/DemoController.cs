22e002463a6434edb83731cdc87f920cebd6ca2b
PhamVuHaiDuong <77629198+PhamVuHaiDuong@users.noreply.github.com>
2021-09-07 05:53:04 +0700

    Done

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TH2.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult TinhTong()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TinhTong(string SoA, string SoB)
+        {
+            //chuyen doi kieu du lieu cau 2 tham so gui tu client len sever
+            double So1 = Convert.ToDouble(SoA);
+            double So2 = Convert.ToDouble(SoB);
+            //tinh tong 2 so
+            double tong = So1 + So2;
+            //su dung viewbag de gui du lieu tu sever ve client
+            ViewBag.TongHaiSo = tong;
+            return View();
+        }
+    }
+}
---
+++ Models/GPT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TH2.Models
+{
+    public class GPT
+    {
+        // xay dung 1 phuong thuc co 2 tham so dau vao la so thuc
+        // tra ve ket qua la tong cua 2 so thuc do
+        public double TinhTong(double a, double b)
+        {
+            double tong = 0;
+            tong = a + b;
+            return tong;
+        }
+    }
+}
